6e5d8bb371ba54fe47b91154e08d76673d306bf1
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2022-05-12 18:52:16 +0100

    Homework 1 Done

---
+++ HW1/Homework1/app/src/main/java/com/example/homework1/MainActivity.java
@@ -0,0 +1,227 @@
+package com.example.homework1;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtPhoneNo;
+    Map<Integer,Contact> speedDials;
+    Button sd1;
+    Button sd2;
+    Button sd3;
+    Button delB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtPhoneNo = (EditText) findViewById(R.id.phoneInput);
+        speedDials = new HashMap<>();
+        speedDials.put(1,null);
+        speedDials.put(2,null);
+        speedDials.put(3,null);
+//        lblinfo = (TextView) findViewById(R.id.lblinfo);
+
+        sd1 = (Button)findViewById(R.id.speeddial1);
+        sd2 = (Button)findViewById(R.id.speeddial2);
+        sd3 = (Button)findViewById(R.id.speeddial3);
+
+        delB = (Button)findViewById(R.id.backspace);
+
+        updateDials();
+
+        sd1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                showDialog("1");
+                return true;
+            }
+        });
+
+        sd2.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                showDialog("2");
+                return true;
+            }
+        });
+
+        sd3.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                showDialog("3");
+                return true;
+            }
+        });
+
+        delB.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                edtPhoneNo.setText("");
+                return true;
+            }
+        });
+    }
+
+    public void buttonClickEvent(View v) {
+        System.out.println("started");
+        String phoneNo = edtPhoneNo.getText().toString();
+        try {
+
+            switch (v.getId()) {
+                case R.id.number0:
+                    phoneNo += "0";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number1:
+                    phoneNo += "1";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number2:
+                    phoneNo += "2";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number3:
+                    phoneNo += "3";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number4:
+                    phoneNo += "4";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number5:
+                    phoneNo += "5";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number6:
+                    phoneNo += "6";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number7:
+                    phoneNo += "7";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number8:
+                    phoneNo += "8";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.number9:
+                    phoneNo += "9";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.numberhash:
+                    phoneNo += "#";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.plusbutton:
+                    phoneNo += "+";
+                    edtPhoneNo.setText(phoneNo);
+                    break;
+                case R.id.backspace:
+                    String copyPhone = phoneNo;
+                    StringBuffer sb= new StringBuffer(copyPhone);
+                    sb.deleteCharAt(sb.length()-1);
+                    edtPhoneNo.setText(sb.toString());
+                    break;
+                case R.id.call:
+                    if (!phoneNo.trim().equals("")) {
+                        Boolean isHash = false;
+                        if (phoneNo.subSequence(phoneNo.length() - 1, phoneNo.length()).equals("#")) {
+                            phoneNo = phoneNo.substring(0, phoneNo.length() - 1);
+                            String callInfo = "tel:" + phoneNo + Uri.encode("#");
+                            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                            callIntent.setData(Uri.parse(callInfo));
+                            startActivity(callIntent);
+                        } else {
+                            String callInfo = "tel:" + phoneNo;
+                            Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                            callIntent.setData(Uri.parse(callInfo));
+                            startActivity(callIntent);
+                        }
+                    }
+                    break;
+                case R.id.speeddial1:
+                    if(speedDials.get(1)!=null)
+                    {
+                        edtPhoneNo.setText(speedDials.get(1).getNumber());
+                    }
+                    break;
+                case R.id.speeddial2:
+                    if(speedDials.get(2)!=null)
+                    {
+                        edtPhoneNo.setText(speedDials.get(2).getNumber());
+                    }
+                    break;
+                case R.id.speeddial3:
+                    if(speedDials.get(3)!=null)
+                    {
+                        edtPhoneNo.setText(speedDials.get(3).getNumber());
+                    }
+                    break;
+            }
+
+        } catch (Exception ex) {
+        }
+
+    }
+
+    public void updateDials() {
+        if(speedDials.get(1)!=null) {
+            sd1.setText(speedDials.get(1).getName());
+        }
+        if(speedDials.get(2)!=null) {
+            sd2.setText(speedDials.get(2).getName());
+        }
+        if(speedDials.get(3)!=null) {
+            sd3.setText(speedDials.get(3).getName());
+        }
+    }
+
+    public void showDialog(String option) {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        adb.setView(inflater.inflate(R.layout.speed_dial2, null));
+        AlertDialog alertDialog = adb.show();
+        Button saveButton = (Button)alertDialog.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String name = ((EditText)alertDialog.findViewById(R.id.nameInput2)).getText().toString();
+                String number = ((EditText)alertDialog.findViewById(R.id.numberInput2)).getText().toString();
+
+                if(name != "" && number != "") {
+                    speedDials.put(Integer.parseInt(option), new Contact(name, number));
+                    alertDialog.cancel();
+                    updateDials();
+
+                }
+            }
+        });
+        Button cancelButton = (Button)alertDialog.findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                alertDialog.cancel();
+            }
+        });
+    }
+
+
+}
